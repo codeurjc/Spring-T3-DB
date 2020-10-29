5860f1a88914fe9243aa5904c81a21f1f4984a3b
oscarsotosanchez <oscarsotosanchez@gmail.com>
2020-10-29 11:11:57 +0100

    Change getmapping by postmapping in a wrong method

--- bbdd_ejem14b/src/main/java/es/codeurjc/daw/TablonController.java
+++ bbdd_ejem14b/src/main/java/es/codeurjc/daw/TablonController.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TablonController {
@@ -46,7 +47,7 @@ public class TablonController {
 		return "tablon";
 	}
 
-	@GetMapping("/anuncio/nuevo")
+	@PostMapping("/anuncio/nuevo")
 	public String nuevoAnuncio(Model model, Anuncio anuncio) {
 
 		repository.save(anuncio);
